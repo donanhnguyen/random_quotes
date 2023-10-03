073aa47cef57e39ea82ae47f38752d7555262c77
Don Anh Nguyen <thedonwind69@gmail.com>
2023-10-02 22:33:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [quote, setQuote] = useState({});
+  const [backgroundColorState, setBackgroundColorState] = useState(getRandomColor());
+  const [nextColor, setNextColor] = useState(getRandomColor());
+  const [autoMode, setAutoMode] = useState(false);
+
+  useEffect(() => {
+    if (autoMode) {
+      const intervalId = setInterval(() => {
+        fetchQuote();
+      }, 3000);
+      return () => {
+        clearInterval(intervalId);
+      };
+    } else {
+      fetchQuote();
+    }
+  }, [autoMode]);
+
+  const fetchQuote = async () => {
+    try {
+      const response = await axios.get('http://safetybelt.pythonanywhere.com/quotes/random');
+      setQuote(response.data);
+      setBackgroundColorState(nextColor);
+      setNextColor(getRandomColor());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  function getRandomColor() {
+    const colors = ['yellow', 'blue', 'gray', 'green', 'purple', 'orange', 'black', 'brown', 'red', 'teal'];
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * colors.length);
+    } while (newIndex === getRandomColor.previousIndex);
+    getRandomColor.previousIndex = newIndex;
+    return colors[newIndex];
+  }
+
+  const quoteContainerStyle = {
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    fontFamily: "Georgia, 'Times New Roman', Times, serif",
+    backgroundImage: `linear-gradient(to bottom right, ${backgroundColorState}, ${nextColor})`,
+    backgroundSize: '200% 200%',
+    transition: 'background-image 1s',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='quote-container' style={quoteContainerStyle}>
+      <div className="quote-box">
+        <p className="quote-text">{quote.quote}</p>
+        <p className="author">{quote.author}</p>
+      </div>
+
+      {!autoMode ? (
+        <button className="generate-button" onClick={fetchQuote}>
+          Generate Quote
+        </button>
+      ) : (
+        ""
+      )}
+
+      <button className="autoMode-button" onClick={() => setAutoMode((prevState) => !prevState)}>
+        {autoMode ? "Manual Mode" : "Set Auto Mode"}
+      </button>
     </div>
   );
 }
